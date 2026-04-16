a98cc82138142b2c52848b154bc8cbc131a05b9a
hematejaswi-30 <hematejaswi29@mail.com>
2026-04-17 00:25:25 +0530

    commit changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
- 
+
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://niveshai-api.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
